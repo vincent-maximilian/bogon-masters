e39f48e09683ad34933a288980d9a7eb186009e7
Ray Novales <rnovales@MacBook-Pro.local>
2015-07-08 20:12:53 -0700

    websocket triumph

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,13 +1,26 @@
 package controllers
 
 import play.api._
+import play.api.libs.iteratee.{Iteratee, Concurrent}
 import play.api.mvc._
 import play.api.libs.json.Json
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 class Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index =  WebSocket.using[String] { request =>
+    //Concurernt.broadcast returns (Enumerator, Concurrent.Channel)
+
+    val (out,channel) = Concurrent.broadcast[String]
+    //log the message to stdout and send response back to client
+
+    val in = Iteratee.foreach[String] {
+      msg => println(msg)
+        //the channel will push to the Enumerator
+        channel push("RESPONSE: " + msg + " you sucka")
+    }
+    (in,out)
   }
 
   def version = Action {
@@ -15,5 +28,13 @@ class Application extends Controller {
       "name" -> "Bogon Masters!",
       "version" -> "0.1"
     )))
+  }
+
+  def hello(name: String) = Action {
+    Ok("Hello " + name)
+  }
+
+  def id(id: Long) = Action {
+    Ok("Howdy " + id)
   }
 }
